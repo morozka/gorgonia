92ba5499cbd4aa35a5209f9c47c713a9dfbe11e5
Dimitrii Lopanov <sexykdi@gmail.com>
2020-06-20 14:54:25 +0300

    printing layers

--- examples/tiny-yolo-v3-coco/yolov3_tiny.go
+++ examples/tiny-yolo-v3-coco/yolov3_tiny.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
+
 	"github.com/pkg/errors"
 	"gorgonia.org/gorgonia"
 	"gorgonia.org/tensor"
@@ -34,7 +38,6 @@ func NewYoloV3Tiny(g *gorgonia.ExprGraph, classesNumber, boxesPerCell int, cfgFi
 	if err != nil {
 		return nil, errors.Wrap(err, "Can't read darknet configuration")
 	}
-	_ = buildingBlocks
 
 	weightsData, err := ParseWeights(weightsFile)
 	if err != nil {
@@ -42,6 +45,175 @@ func NewYoloV3Tiny(g *gorgonia.ExprGraph, classesNumber, boxesPerCell int, cfgFi
 	}
 	_ = weightsData
 
+	for i := range buildingBlocks {
+		layerType, ok := buildingBlocks[i]["type"]
+		if ok {
+			switch layerType {
+			case "convolutional":
+				filters := 0
+				padding := 0
+				kernelSize := 0
+				stride := 0
+				batchNormalize := 0
+				bias := false
+				activation := "activation"
+				activation, ok := buildingBlocks[i]["activation"]
+				if !ok {
+					fmt.Printf("No field 'activation' for convolution layer")
+					continue
+				}
+				batchNormalizeStr, ok := buildingBlocks[i]["batch_normalize"]
+				batchNormalize, err := strconv.Atoi(batchNormalizeStr)
+				if !ok || err != nil {
+					batchNormalize = 0
+					bias = true
+				}
+				filtersStr, ok := buildingBlocks[i]["filters"]
+				filters, err = strconv.Atoi(filtersStr)
+				if !ok || err != nil {
+					fmt.Printf("Wrong or empty 'filters' parameter for convolution layer: %s\n", err.Error())
+					continue
+				}
+				paddingStr, ok := buildingBlocks[i]["pad"]
+				padding, err = strconv.Atoi(paddingStr)
+				if !ok || err != nil {
+					fmt.Printf("Wrong or empty 'pad' parameter for convolution layer: %s\n", err.Error())
+					continue
+				}
+				kernelSizeStr, ok := buildingBlocks[i]["size"]
+				kernelSize, err = strconv.Atoi(kernelSizeStr)
+				if !ok || err != nil {
+					fmt.Printf("Wrong or empty 'size' parameter for convolution layer: %s\n", err.Error())
+					continue
+				}
+				strideStr, ok := buildingBlocks[i]["stride"]
+				stride, err = strconv.Atoi(strideStr)
+				if !ok || err != nil {
+					fmt.Printf("Wrong or empty 'stride' parameter for convolution layer: %s\n", err.Error())
+					continue
+				}
+
+				fmt.Printf(
+					"Convolution layer: Filters->%[1]d Padding->%[2]d Kernel->%[3]dx%[3]d Stride->%[4]d Activation->%[5]s Batch->%[6]d Bias->%[7]t\n",
+					filters, padding, kernelSize, stride, activation, batchNormalize, bias,
+				)
+				break
+			case "upsample":
+				scale := 0
+				scaleStr, ok := buildingBlocks[i]["stride"]
+				scale, err = strconv.Atoi(scaleStr)
+				if !ok || err != nil {
+					fmt.Printf("Wrong or empty 'stride' parameter for upsampling layer: %s\n", err.Error())
+					continue
+				}
+				fmt.Printf(
+					"Upsample layer: Scale->%[1]d\n",
+					scale,
+				)
+				break
+			case "route":
+				routeLayersStr, ok := buildingBlocks[i]["layers"]
+				if !ok {
+					fmt.Printf("No field 'layers' for route layer")
+					continue
+				}
+				layersSplit := strings.Split(routeLayersStr, ",")
+				if len(layersSplit) < 1 {
+					fmt.Printf("Something wrong with route layer. Check if it has one array item atleast")
+					continue
+				}
+				for l := range layersSplit {
+					layersSplit[l] = strings.TrimSpace(layersSplit[l])
+				}
+				start := 0
+				end := 0
+				start, err := strconv.Atoi(layersSplit[0])
+				if err != nil {
+					fmt.Printf("Each first element of 'layers' parameter for route layer should be an integer: %s\n", err.Error())
+					continue
+				}
+				if len(layersSplit) > 1 {
+					end, err = strconv.Atoi(layersSplit[1])
+					if err != nil {
+						fmt.Printf("Each second element of 'layers' parameter for route layer should be an integer: %s\n", err.Error())
+						continue
+					}
+				}
+				if start > 0 {
+					start = start - i
+				}
+				if end > 0 {
+					end = end - i
+				}
+				if end < 0 {
+					fmt.Printf(
+						"Route layer: Start->%[1]d End->%[2]d\n",
+						i+start, i+end,
+					)
+				} else {
+					fmt.Printf(
+						"Route layer: Start->%[1]d\n",
+						i+start,
+					)
+				}
+				break
+			case "yolo":
+				maskStr, ok := buildingBlocks[i]["mask"]
+				if !ok {
+					fmt.Printf("No field 'mask' for YOLO layer")
+					continue
+				}
+				maskSplit := strings.Split(maskStr, ",")
+				if len(maskSplit) < 1 {
+					fmt.Printf("Something wrong with yolo layer. Check if it has one item in 'mask' array atleast")
+					continue
+				}
+				masks := make([]int, len(maskSplit))
+				for l := range maskSplit {
+					maskSplit[l] = strings.TrimSpace(maskSplit[l])
+					masks[l], err = strconv.Atoi(maskSplit[l])
+					if err != nil {
+						fmt.Printf("Each element of 'mask' parameter for yolo layer should be an integer: %s\n", err.Error())
+					}
+				}
+				anchorsStr, ok := buildingBlocks[i]["anchors"]
+				if !ok {
+					fmt.Printf("No field 'anchors' for YOLO layer")
+					continue
+				}
+				anchorsSplit := strings.Split(anchorsStr, ",")
+				if len(anchorsSplit) < 1 {
+					fmt.Printf("Something wrong with yolo layer. Check if it has one item in 'anchors' array atleast")
+					continue
+				}
+				if len(anchorsSplit)%2 != 0 {
+					fmt.Printf("Number of elemnts in 'anchors' parameter for yolo layer should be divided exactly by 2 (even number)")
+					continue
+				}
+				anchors := make([]int, len(anchorsSplit))
+				for l := range anchorsSplit {
+					anchorsSplit[l] = strings.TrimSpace(anchorsSplit[l])
+					anchors[l], err = strconv.Atoi(anchorsSplit[l])
+					if err != nil {
+						fmt.Printf("Each element of 'anchors' parameter for yolo layer should be an integer: %s\n", err.Error())
+					}
+				}
+				anchorsPairs := [][2]int{}
+				for a := 0; a < len(anchors); a += 2 {
+					anchorsPairs = append(anchorsPairs, [2]int{anchors[a], anchors[a+1]})
+				}
+				fmt.Printf("YOLO layer: ")
+				for m := range masks {
+					fmt.Printf("Mask->%[1]d Anchors->[%[2]d, %[3]d]  |  ", masks[m], anchorsPairs[m][0], anchorsPairs[m][1])
+				}
+				fmt.Println()
+				break
+			default:
+				break
+			}
+		}
+
+	}
 	lastIdx := 5 // skip first 5 values
 	epsilon := float32(0.000001)
 
